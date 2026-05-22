571848f119b29287c66657250a6fbfe2cb9ceb4d
Niffirg-DcM <mcdonald.w.griffin@gmail.com>
2026-05-21 20:29:07 -0700

    Updating Parser reads
    started on remaking parser to account for various input styles.

--- src/Parser.cpp
+++ src/Parser.cpp
@@ -5,10 +5,10 @@
 //  Created by Griffin McDonald on 2026-05-10.
 //
 #include <iostream>
-#include <sstream>
 #include <cctype>
 #include <stack>
 #include "../include/Parser.hpp"
+#include <regex>
 
 //Converts raw string into RPN queue
 std::queue<Token> Parser::parseToPostfix(const std::string& expression) {
@@ -52,40 +52,76 @@ std::queue<Token> Parser::parseToPostfix(const std::string& expression) {
 
 //Breaks string into individual units (numbers, ops, parens)
 std::vector<Token> Parser::tokenize(const std::string& expression) {
+    //Trash Code...
+    // std::vector<Token> tokens;
+    // std::vector<std::string> hold;
+    // std::stringstream ss(expression);
+    // std::string segment;
+
+    // while (ss >> segment) {
+    //     hold.push_back(segment);
+    // }
+
+    // for (int i =0;i<hold.size();i++){
+    //     Token t;
+    //     int op;
+    //     if (hold[i].size() ==1) {
+    //         op = isOperator(hold[i][0]);
+    //         if (op!=0) {
+    //             t.type = TokenType::Operator;
+    //             t.symbol = hold[i];
+    //             t.precedence = op;
+    //         } else {
+    //             t.type = TokenType::Number;
+    //             t.value = std::strtod(hold[i].c_str(), nullptr);
+    //         }
+    //     } else {
+    //         op = isFunction(hold[i]);
+    //         if (op!=0) {
+    //             t.type = TokenType::Function;
+    //             t.symbol = hold[i];
+    //         } else {
+    //             t.type = TokenType::Number;
+    //             t.value = std::strtod(hold[i].c_str(), nullptr);
+    //         }
+    //     }
+    //     tokens.push_back(t);
+    // }
+
     std::vector<Token> tokens;
-    std::vector<std::string> hold;
-    std::stringstream ss(expression);
-    std::string segment;
 
-    while (ss >> segment) {
-        hold.push_back(segment);
-    }
+    /*
+    Index from start
+    maintain first separately and use current index.
+    when no longer digit make it an int and pass it to tokens.
+    when switches from non int to int, pass it to tokens
+    */
+   int last =0;
+   std::regex e("^[-+]?[0-9]*\\.?[0-9]+$");
 
-    for (int i =0;i<hold.size();i++){
+    for (int i = 1; i < expressions.length(); i++) {
         Token t;
-        int op;
-        if (hold[i].size() ==1) {
-            op = isOperator(hold[i][0]);
-            if (op!=0) {
-                t.type = TokenType::Operator;
-                t.symbol = hold[i];
-                t.precedence = op;
+        //if substring expressions at i is digit and substring from last to i-1 isnt digit, turn last:i-1 into token of paren/op/func type
+        //if substring expressions at i isn't digit and substring from last to i-1 is digit, turn last:i-1 into token of number type
+        if (std::regex_match(expressions.substring(last, i-last-1)),e) {
+            if (std::regex_match(expressions.substring(last, i-last)),e) {
+                continue;
             } else {
+                t.value = double(expressions.substring(last, i-1-last));
                 t.type = TokenType::Number;
-                t.value = std::strtod(hold[i].c_str(), nullptr);
             }
         } else {
-            op = isFunction(hold[i]);
-            if (op!=0) {
-                t.type = TokenType::Function;
-                t.symbol = hold[i];
+            if (std::regex_match(expressions.substring(i, 1)),e) {
+                std::string hold = expressions.substring(last, i-1-last);
+                t.type = TokenType::operation;
+
             } else {
-                t.type = TokenType::Number;
-                t.value = std::strtod(hold[i].c_str(), nullptr);
+                continue;
             }
         }
-        tokens.push_back(t);
     }
+
+
     return tokens;
 }
 
